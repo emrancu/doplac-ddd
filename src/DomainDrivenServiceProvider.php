734df8b0f5446b0b2ba1991e882693434da23871
AL EMRAN <emrancu1@gmail.com>
2023-08-21 17:17:00 +0600

    Update DomainDrivenServiceProvider.php

--- src/DomainDrivenServiceProvider.php
+++ src/DomainDrivenServiceProvider.php
@@ -18,8 +18,7 @@ class DomainDrivenServiceProvider extends ServiceProvider
          * set configuration file support from all domain with domain name's snake case prefix. like config('email_marketing.services')
          * and also set language file support from all domains with domain name's snake case namespace. Like: trans('data_store::campaign')
          */
-        if (! app()->configurationIsCached()) {
-
+        if (!app()->configurationIsCached()) {
             foreach ($support->getDomains() as $domain) {
                 if ($domain['title'] === 'app') {
                     continue;
@@ -30,7 +29,6 @@ class DomainDrivenServiceProvider extends ServiceProvider
                 $files = File::allFiles($domain['real_path'].'../config');
 
                 foreach ($files as $file) {
-
                     $configName = explode('.', $file->getFilename())[0] ?? null;
 
                     Config::set("$baseName.$configName", require $file->getRealPath());
@@ -39,16 +37,57 @@ class DomainDrivenServiceProvider extends ServiceProvider
         }
 
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+
+        foreach ($support->getDomains() as $domain) {
+            if ($domain['title'] === 'app') {
+                continue;
+            }
+
+            if (is_dir($domain['real_path'].'/Providers')) {
+                foreach (File::allFiles($domain['real_path'].'/Providers') as $file) {
+                    $fileName = explode('.', $file->getFilename())[0];
+                    app()->register('\\'.$domain['namespace'].'Providers\\'.$fileName);
+                }
+            }
+        }
+
+        if ($this->app->runningInConsole()) {
+            $comands = [];
+
+            foreach ($support->getDomains() as $domain) {
+                if ($domain['title'] === 'app') {
+                    continue;
+                }
+
+                if (is_dir($domain['real_path'].'/Providers')) {
+                    foreach (File::allFiles($domain['real_path'].'/Providers') as $file) {
+                        $fileName = explode('.', $file->getFilename())[0];
+                        app()->register('\\'.$domain['namespace'].'Providers\\'.$fileName);
+                    }
+                }
+
+                if (is_dir($domain['real_path'].'/Console/Commands')) {
+                    if (is_file($domain['real_path'].'/Console/Kernel.php')) {
+                        app()->make('\\'.$domain['namespace'].'Console\\Kernel');
+                    }
+
+                    foreach (File::allFiles($domain['real_path'].'/Console/Commands') as $file) {
+                        $fileName = explode('.', $file->getFilename())[0];
+                        $comands[] = '\\'.$domain['namespace'].'Console\\Commands\\'.$fileName;
+                    }
+                }
+            }
+
+            $this->commands([
+                \ZupiterDoplac\Domain\Commands\DomainMigration::class,
+                \ZupiterDoplac\Domain\Commands\DomainSeed::class,
+                ...$comands
+            ]);
+        }
     }
 
     public function register()
     {
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                \ZupiterDoplac\Domain\Commands\DomainMigration::class,
-                \ZupiterDoplac\Domain\Commands\DomainSeed::class,
-            ]);
-        }
 
     }
 
